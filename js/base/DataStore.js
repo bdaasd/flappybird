efb8ddd2fb15ab5b453f8ab7bdc7be1acc891e97
Bdaasd <1183628293@qq.com>
2019-12-13 17:56:37 +0800

    day1pm结束 定义资源数组,定义资源加载器.定义变量池.在Main中初始化数据,加载数据

---
+++ js/base/DataStore.js
@@ -0,0 +1,45 @@
+/**
+ * 变量池,用于保存游戏过程中各种数据
+ * 方便我们在不同的类中访问和修改变量
+ */
+export class DataStore{
+   constructor(){
+       console.log('创建一个变量池');
+       this.map = new Map();
+   }
+   /**
+    * 单例 整个游戏过程中只能出现一个变量池
+    * 必须保证变量池的唯一(只有一个变量池)
+    */
+   static getInstance(){
+        if(!DataStore.instance){
+            //没有实例 instance 实例
+            DataStore.instance = new DataStore();
+        }
+        return DataStore.instance;
+   }
+   /**
+    * 根据给定的key和val 保存数据
+    * @param {String} key 
+    * @param {Oblect} val 
+    */
+   put(key,val){
+       this.map.set(key,val);
+       return this;//方便链式调用
+   }
+   /**
+    * 根据给定的key获取对应的值
+    * @param {String} key 
+    */
+   get(key){
+       return this.map.get(key);
+   }
+   /**
+    * 游戏结束,销毁数据
+    */
+   destroy(){
+       for(let val of this.map.values()){
+           val = null;
+       }
+   }
+}
